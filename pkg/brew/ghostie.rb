c8ce0b700440ad1d7a7520f8ce43a88116b4bb9b
Aayush Attri <aayush.attri@snyk.io>
2023-01-07 11:25:33 +0000

    Update Homebrew

--- pkg/brew/ghostie.rb
+++ pkg/brew/ghostie.rb
@@ -1,16 +1,16 @@
 class Ghostie < Formula
   desc "Github notifications in your terminal"
   homepage "https://github.com/attriaayush/ghostie"
-  version "v0.2.1"
+  version "v0.3.0"
 
   depends_on "sqlite"
 
   if OS.mac?
     url "https://github.com/attriaayush/ghostie/releases/download/#{version}/ghostie-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "09b977764f9ce68d1415103da7bb38b80e1c7cc640df7ac0a88ea941dfbaa764"
+    sha256 "c20c454a778ba76b27460216df5cf451128223cb5ecf4a4a1d0625bf730881c7"
   elsif OS.linux?
     url "https://github.com/attriaayush/ghostie/releases/download/#{version}/ghostie-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "bb003d8054329818199c290df190dbade7b94e7d62fff126555cd21ec1a2984e"
+    sha256 "db071cc275adfeb2384dec3da6190c399bca8974add9b99c7196dd6f2ae37fcc"
   end
 
   def install
